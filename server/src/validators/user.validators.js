145988e873617afb57e8c8c030ce9e9bd978334b
Nithin Raju <nithinraju242@gmail.com>
2025-02-21 06:02:25 +0530

    login user server side

--- server/src/validators/user.validators.js
+++ server/src/validators/user.validators.js
@@ -1,6 +1,6 @@
 import { body, validationResult } from "express-validator";
 
-// Define validation rules of user
+// Define validation rules of register user
 export const registerValidationRules = [
   body("fullName.firstName")
     .isLength({ min: 3 })
@@ -15,11 +15,24 @@ export const registerValidationRules = [
     .withMessage("Password must be at least 8 characters long"),
 ];
 
+// Define validation rules of login user
+export const loginValidationRules = [
+  body("email").isEmail().withMessage("Email is invalid"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long"),
+];
+
 // Middleware to check validation errors
 export const validate = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ success: false, errors: errors.array() });
+    const errorMessages = errors.array().map((error) => error.msg);
+
+    return res.status(400).json({
+      success: false,
+      errors: errorMessages.length === 1 ? errorMessages[0] : errorMessages,
+    });
   }
   next();
 };
